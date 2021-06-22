605d72674696b9c630716953f74ece8a0fd23317
Chay Williams <Chayvw@gmail.com>
2021-06-22 14:46:14 -0400

    updated server.js to serve react build folder

--- client/src/Containers/Search.jsx
+++ client/src/Containers/Search.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+// import { response } from 'express';
 
 class Search extends Component {
     constructor(){
         super()
 
         this.state={
-            userInput: ""
+            userInput: "",
+            results: ""
         }
+
+        }
+        handleUserInput = (event) =>{
+            event.preventDefault();
+            const query = this.state.userInput;
+            const url = `https://api.edamam.com/search?q=chicken&ap_id=c06c8ad2&app_key=02e54c7f11fc157d0fef77be1af9f2ef`
+            axios({
+                url:url,
+                method:'GET'
+            }).then((response)=>{
+                console.log(response.data)
+                this.setState({
+                    results: response.data
+
+                }).catch(()=>{
+                    alert("Error")
+                })
+            })
+        }
+
+        captureUserInput = (input) => {
+            this.setState({
+                userInput: input
+            }, () => { console.log(input) })
+        
 
 
     }
@@ -14,6 +42,8 @@ class Search extends Component {
         return (
             <div>
                <h1>Search Page</h1> 
+               <input type="text"  placeholder="Reciepe Search" value={this.state.userInput} onChange={(event) => { this.captureUserInput(event.target.value) }} />
+               <button onClick={this.handleUserInput}>Search</button>
             </div>
         );
     }
